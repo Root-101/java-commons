2483c1ebbb61cb3e020e83bac5a2e56822da7737
Unknown <unknown@example.com>
2020-06-12 22:55:57 -0500

    Creado el Checker never para cuando se quiere poner directo un error

---
+++ src/main/java/com/clean/core/utils/validation/checkables/NeverCheckable.java
@@ -0,0 +1,25 @@
+package com.clean.core.utils.validation.checkables;
+
+/**
+ *
+ * @author Jesús Hernández Barrios (jhernandezb96@gmail.com)
+ */
+public class NotNullCheckable implements Checkable {
+
+    private Object source;
+
+    public NotNullCheckable(Object source) {
+        this.source = source;
+    }
+
+    @Override
+    public boolean check() {
+        return source != null;
+    }
+
+    @Override
+    public Object getSource() {
+        return source;
+    }
+
+}

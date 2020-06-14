75645a5d32985fd46fa592e91dc222041a9d8c68
Unknown <unknown@example.com>
2020-06-14 00:19:13 -0500

    Creada annotation personalizable

--- src/main/java/com/clean/core/utils/validation/annotations/Digit.java
+++ src/main/java/com/clean/core/utils/validation/annotations/Digit.java
@@ -5,6 +5,8 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
 
 /**
  *
@@ -12,9 +14,14 @@ import java.lang.annotation.Target;
  */
 @Target(value = ElementType.FIELD)
 @Retention(value = RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = DigitRegister.class)
 public @interface Digit {
 
-    public String message();
+    String message() default "";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 
     public String detailMessage() default "";
 
--- src/test/java/com/clean/DomainObjectExample.java
+++ src/test/java/com/clean/DomainObjectExample.java
@@ -1,8 +1,8 @@
 package com.clean;
 
-
 import com.clean.core.utils.validation.Validable;
 import com.clean.core.utils.validation.ValidationResult;
+import com.clean.core.utils.validation.annotations.Digit;
 import com.clean.core.utils.validation.checkables.CheckerFactory;
 import javax.validation.Validation;
 import javax.validation.ValidationException;
@@ -14,20 +14,23 @@ import javax.validation.constraints.NotNull;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
- * 
+ *
  * JoBits
+ *
  * @author Jorge
- * 
+ *
  */
-public class DomainObjectExample  implements Validable{
+public class DomainObjectExample implements Validable {
 
     private int cant = 8;
-    
+
     @NotNull(message = "Sting no vacio Annotation")
-    private String hola = null;
-    
+    private String hola = "asd";
+
+    @Digit(message = "esto no es un digito")
+    private char c = '1';
+
     @Override
     public ValidationResult validate() throws ValidationException {
         ValidationResult ret = new ValidationResult();
@@ -35,7 +38,7 @@ public class DomainObjectExample  implements Validable{
         ret.check(this);
         return ret.throwException();
     }
-    
+
     public static void main(String[] args) {
         new DomainObjectExample().validate();
     }

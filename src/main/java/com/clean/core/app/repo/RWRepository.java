0858d1fcf06d2a61ad4448a9a46339d9a02e61a9
Unknown <unknown@example.com>
2020-06-23 13:42:36 -0500

    Creado el repo RW para operaciones simples de read-write

---
+++ src/main/java/com/clean/core/app/repo/RWRepository.java
@@ -0,0 +1,14 @@
+package com.clean.core.app.repo;
+
+/**
+ *
+ * @author Jorge
+ * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ * @param <T>
+ */
+public interface RWRepository<T> extends AbstractRepository {
+
+    public T read() throws Exception;
+
+    public void write(T object) throws Exception;
+}

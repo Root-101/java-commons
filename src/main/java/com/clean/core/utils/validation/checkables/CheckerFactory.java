831b635113a8d9e048150f109194ecc8454b3f56
Unknown <unknown@example.com>
2020-08-08 18:29:49 -0500

    CheckerFactory: + source. Agergado el source a los build

--- src/main/java/com/clean/core/utils/validation/checkables/CheckerFactory.java
+++ src/main/java/com/clean/core/utils/validation/checkables/CheckerFactory.java
@@ -5,21 +5,21 @@ import com.clean.core.utils.validation.checkables.impl.NeverCheckable;
 import com.clean.core.utils.validation.checkables.impl.SizeExactCheckable;
 
 /**
- * 
+ *
  * @author Jorge
  * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
  */
 public class CheckerFactory {
 
-    public static Checkable buildDigitCheckable(Character source) {
-        return new DigitCheckable(source);
+    public static Checkable buildDigitCheckable(String source, Character value) {
+        return new DigitCheckable(source, value);
     }
 
-    public static Checkable buildLengthExactCheckable(String source, int length) {
-        return new SizeExactCheckable(source, length);
+    public static Checkable buildLengthExactCheckable(String source, String value, int length) {
+        return new SizeExactCheckable(source, value, length);
     }
 
-    public static Checkable buildNeverCheckable(Object source) {
-        return new NeverCheckable(source);
+    public static Checkable buildNeverCheckable(String source, Object value) {
+        return new NeverCheckable(source, value);
     }
 }

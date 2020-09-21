7422d81e56b907ec219e9d97c9393d07b92dfbb7
Unknown <unknown@example.com>
2020-09-21 12:40:48 -0500

    CRUDRepository: +PropertyChangeSupport

--- src/main/java/com/clean/core/app/usecase/ReadWriteUseCase.java
+++ src/main/java/com/clean/core/app/usecase/ReadWriteUseCase.java
@@ -14,4 +14,8 @@ public interface ReadWriteUseCase<T> extends AbstractUseCase {
     public T read() throws Exception;
 
     public void write(T object) throws Exception;
+
+    public void addPropertyChangeListener(java.beans.PropertyChangeListener listener);
+
+    public void removePropertyChangeListener(java.beans.PropertyChangeListener listener);
 }

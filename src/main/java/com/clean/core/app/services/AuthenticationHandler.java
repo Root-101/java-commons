4fbeda488410374c68e11b63f9d360384d458d8f
Unknown <unknown@example.com>
2020-12-17 16:40:08 -0500

    AuthenticationHandler: +static.login

--- src/main/java/com/clean/core/app/services/AuthenticationHandler.java
+++ src/main/java/com/clean/core/app/services/AuthenticationHandler.java
@@ -1,25 +1,38 @@
 package com.clean.core.app.services;
 
+import com.clean.core.exceptions.AlreadyRegisteredService;
+import java.util.Map;
+
 /**
- * 
+ *
  * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
  */
 public class AuthenticationHandler {
 
-    private static AuthenticationHandlerService loginHandlerService;
+    private static AuthenticationHandlerService authHandlerService;
 
     private AuthenticationHandler() {
     }
 
-    public static void registerLoginHandlerService(AuthenticationHandlerService newService) {
-        loginHandlerService = newService;
+    public static void registerAuthHandlerService(AuthenticationHandlerService newService) {
+        if (authHandlerService != null) {
+            throw new AlreadyRegisteredService("Authentication");
+        }
+        authHandlerService = newService;
     }
 
-    public static AuthenticationHandlerService getLoginHandlerService() {
-        if (loginHandlerService == null) {
+    public static AuthenticationHandlerService getAuthHandlerService() {
+        if (authHandlerService == null) {
             throw new IllegalStateException("Bad call");
         }
-        return loginHandlerService;
+        return authHandlerService;
     }
 
+    public static <T> T login(Object user, Object pass) {
+        return (T) getAuthHandlerService().login(user, pass);
+    }
+
+    public static <T> T login(Object user, Object pass, Map<String, Object> args) {
+        return (T) getAuthHandlerService().login(user, pass, args);
+    }
 }

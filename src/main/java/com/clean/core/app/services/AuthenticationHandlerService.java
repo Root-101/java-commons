f93e0b0306933eb8f9379318760db4cc76054595
Unknown <unknown@example.com>
2020-12-19 20:29:45 -0500

    Authentication: +logout

--- src/main/java/com/clean/core/app/services/AuthenticationHandlerService.java
+++ src/main/java/com/clean/core/app/services/AuthenticationHandlerService.java
@@ -8,16 +8,17 @@ import java.util.Map;
  * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
  * @param <UserType>
  * @param <PassType>
- * @param <AnswType>
  * @param <LogedUserType>
  */
-public interface AuthenticationHandlerService<UserType, PassType, AnswType, LogedUserType> {
+public interface AuthenticationHandlerService<UserType, PassType, LogedUserType> {
 
-    public default AnswType login(UserType user, PassType pass) {
+    public default boolean login(UserType user, PassType pass) {
         return login(user, pass, new HashMap<>());
     }
 
-    public AnswType login(UserType user, PassType pass, Map<String, Object> args);
+    public boolean login(UserType user, PassType pass, Map<String, Object> args);
+
+    public boolean logout();
 
     //public boolean checkAccess(Method method){throw }
 }

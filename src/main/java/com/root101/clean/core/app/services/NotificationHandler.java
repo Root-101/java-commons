4ff21d781ecfb1e7da9f25f6d670c439800bee7e
Unknown <unknown@example.com>
2021-01-04 14:23:01 -0500

    NotificationHandler: +sout para consola

--- src/main/java/com/root101/clean/core/app/services/NotificationHandler.java
+++ src/main/java/com/root101/clean/core/app/services/NotificationHandler.java
@@ -40,6 +40,7 @@ public class NotificationHandler {
     }
 
     public static void showNotification(String type, Object textToDisplay) {
+        System.out.println("Notification: " + type + " => " + textToDisplay);
         for (NotificationService notif : notificationServices) {
             notif.showNotification(type, textToDisplay);
         }
@@ -50,6 +51,7 @@ public class NotificationHandler {
     }
 
     public static boolean showConfirmDialog(String type, Object textToDisplay) {
+        System.out.println("Confirm: " + type + " => " + textToDisplay);
         for (NotificationService notif : notificationServices) {
             if (notif.contain(type)) {
                 return notif.showConfirmDialog(type, textToDisplay);
@@ -63,6 +65,7 @@ public class NotificationHandler {
     }
 
     public static Object showInputDialog(String type, Object textToDisplay) {
+        System.out.println("Input: " + type + " => " + textToDisplay);
         for (NotificationService notif : notificationServices) {
             if (notif.contain(type)) {
                 return notif.showInputDialog(type, textToDisplay);

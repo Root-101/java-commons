6260ffacb14770a69a2f2afb9fa7abf2e84a8afd
Jesus Hdez <jhernandezb96@gmail.co>
2023-01-12 23:09:57 -0500

    Removed old architecture standards

--- src/main/java/dev/root101/clean/core/rest/RestTemplateUtils.java
+++ src/main/java/dev/root101/clean/core/rest/RestTemplateUtils.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.root101.clean.core.spring_boot.web.client;
+package dev.root101.clean.core.rest;
 
 import dev.root101.clean.core.utils.jackson.JACKSON;
 import java.util.HashMap;

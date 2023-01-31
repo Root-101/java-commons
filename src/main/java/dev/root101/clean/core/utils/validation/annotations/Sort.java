634c920288084d763ca64462950e0290c1a7111a
Jesus Hdez <jhernandezb96@gmail.co>
2023-01-31 17:04:51 -0500

    +Sort validation

---
+++ src/main/java/dev/root101/clean/core/utils/validation/annotations/Sort.java
@@ -0,0 +1,23 @@
+package dev.root101.clean.core.utils.validation.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+@Target(value = ElementType.FIELD)
+@Retention(value = RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = SortRegister.class)
+public @interface Sort {
+
+    String message() default "Sort type must be 'ASC', 'DESC' or '0' for no sorting.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    public String detailMessage() default "";
+
+}
---
+++ src/main/java/dev/root101/clean/core/utils/validation/annotations/SortRegister.java
@@ -0,0 +1,21 @@
+package dev.root101.clean.core.utils.validation.annotations;
+
+import java.util.Map;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class SortRegister implements ConstraintValidator<Sort, Map<String, String>> {
+
+    @Override
+    public void initialize(Sort a) {
+        ConstraintValidator.super.initialize(a);
+    }
+
+    @Override
+    public boolean isValid(Map<String, String> sort, ConstraintValidatorContext cvc) {
+        return sort == null || sort.isEmpty() || sort.values().stream().anyMatch((t) -> {
+            return SortType.contain(t);
+        });
+    }
+
+}
